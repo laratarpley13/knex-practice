eb5d549bcc45484e98c5ed3e3656b2fe64687ce5
laratarpley13 <ltarp13@gmail.com>
2021-02-06 15:32:40 -0600

    added practice drills

---
+++ src/drills.js
@@ -0,0 +1,63 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+    client: 'pg',
+    connection: process.env.DB_URL
+})
+
+function getItemsWithText(searchTerm) {
+    knexInstance
+        .select('id', 'name', 'price', 'category')
+        .from('shopping_list')
+        .where('name', 'ILIKE', `%${searchTerm}%`)
+        .then(result => {
+            console.log(result)
+        })    
+}
+
+getItemsWithText('urger')
+
+function paginateItems(pageNumber) {
+    const productsPerPage = 6
+    const offset = productsPerPage * (pageNumber - 1)
+    knexInstance
+        .select('id', 'name', 'price', 'category')
+        .from('shopping_list')
+        .limit(productsPerPage)
+        .offset(offset)
+        .then(result => {
+            console.log(result)
+        })
+}
+
+paginateItems(2)
+
+function getItemsAfterDate(daysAgo) {
+    knexInstance
+        .select('id', 'name', 'price', 'date_added', 'category')
+        .where(
+            'date_added',
+            '>',
+            knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo)
+        )
+        .from('shopping_list')
+        .then(result => {
+            console.log(result)
+        })
+}
+
+getItemsAfterDate(5)
+
+function getTotalCostByCategory () {
+    knexInstance
+        .select('category')
+        .sum('price as total')
+        .from('shopping_list')
+        .groupBy('category')
+        .then(result => {
+            console.log(result)
+        })
+}
+
+getTotalCostByCategory()
--- src/practice.js
+++ src/practice.js
@@ -6,7 +6,74 @@ const knexInstance = knex({
     connection: process.env.DB_URL
 })
 
-cosnt q1 = knexInstance.from('amazong_products').select('*').toQuery;
+const qry = knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .where({ name: 'Point of view gun' })
+    .first()
+    .toQuery()
 
-knexInstance('amazong_products')
-    .select('*');
+console.log(qry)
+
+const searchTerm = 'holo'
+
+function searchByProduceName(searchTerm) {
+    knexInstance
+        .select('product_id', 'name', 'price', 'category')
+        .from('amazong_products')
+        .where('name', 'ILIKE', `%${searchTerm}%`)
+        .then(result => {
+            console.log(result)
+    })
+}
+
+//searchByProduceName('holo');
+
+function paginateProducts(page) {
+    const productsPerPage = 10
+    const offset = productsPerPage * (page - 1)
+    knexInstance
+      .select('product_id', 'name', 'price', 'category')
+      .from('amazong_products')
+      .limit(productsPerPage)
+      .offset(offset)
+      .then(result => {
+        console.log(result)
+      })
+  }
+  
+  //paginateProducts(2)
+
+  function getProductsWithImages() {
+    knexInstance
+      .select('product_id', 'name', 'price', 'category', 'image')
+      .from('amazong_products')
+      .whereNotNull('image')
+      .then(result => {
+        console.log(result)
+      })
+  }
+  
+//getProductsWithImages()
+
+function mostPopularVideosForDays(days) {
+    knexInstance
+      .select('video_name', 'region')
+      .count('date_viewed AS views')
+      .where(
+        'date_viewed',
+        '>',
+        knexInstance.raw(`now() - '?? days'::INTERVAL`, days)
+      )
+      .from('whopipe_video_views')
+      .groupBy('video_name', 'region')
+      .orderBy([
+        { column: 'region', order: 'ASC' },
+        { column: 'views', order: 'DESC' },
+      ])
+      .then(result => {
+        console.log(result)
+      })
+  }
+  
+  mostPopularVideosForDays(30)
